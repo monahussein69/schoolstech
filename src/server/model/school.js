032499bc51b7eb3c893a5a35fc4556d4374cf3bb
monahussein <monahussein69@gmail.com>
2018-02-12 07:25:23 +0000

    school.js edited online with Bitbucket

--- src/server/model/school.js
+++ src/server/model/school.js
@@ -2,7 +2,96 @@ var con = require('../routes/dbConfig.js');
 
 var schoolMethods = {
     saveSchool: function(req,res,callback) {
+     var schoolData = req.body.schoolData;
+     var response = {};
+     if(schoolData.schoolId){
+       con.query("select * from SCH_School where id = '?'",[schoolData.schoolId],function(err,result){
+         if(err)
+          throw err;
+         if (Object.keys(result).length){
+          con.query(" update SCH_School set name = '?' , gender = '?',educationLevel='?', address='?',totalClasses='?' ,totalStudents ='?',totalStaff='?',rentedBuildings='?',governmentBuildings='?',foundationYear='?',logoFile='?' where id = '?'",
+          [schoolData.name , 
+           schoolData.gender,
+           schoolData.educationLevel,
+           schoolData.address,
+           schoolData.totalClasses,
+           schoolData.totalStudents,
+           schoolData.totalStaff,
+           schoolData.rentedBuildings,
+           schoolData.governmentBuildings,
+           schoolData.foundationYear,
+           schoolData.logoFile,
+           schoolData.schoolId,
+           ],function(err,result){
+            if(err)
+             throw err
+            if(result.affectedRows){
+             response.success = true;
+			 response.msg = 'تم التعديل بنجاح'
+            }else{
+             response.success = false;
+			 response.msg = 'خطأ , الرجاء المحاوله مره اخرى';
+            } 
+			
+			callback(response);
+			
+           } 
+          );
+         }else{
+		  response.success = false;
+		  response.msg = 'لا يمكن العثور على المدرسه , الرجاء المحاوله مره اخري';
+		  callback(response);
+		 } 
+       });
+     }else{
+	 
+	   con.query(" insert into SCH_School  (name, gender,educationLevel, address,totalClasses ,totalStudents ,totalStaff,rentedBuildings,governmentBuildings,foundationYear,logoFile) values('?','?','?','?','?','?','?','?','?','?','?')",
+          [schoolData.name , 
+           schoolData.gender,
+           schoolData.educationLevel,
+           schoolData.address,
+           schoolData.totalClasses,
+           schoolData.totalStudents,
+           schoolData.totalStaff,
+           schoolData.rentedBuildings,
+           schoolData.governmentBuildings,
+           schoolData.foundationYear,
+           schoolData.logoFile
+           ],function(err,result){
+            if(err)
+             throw err
+            if(result.affectedRows){
+             response.success = true;
+			 response.msg = 'تم الاضافه بنجاح'
+            }else{
+             response.success = false;
+			 response.msg = 'خطأ , الرجاء المحاوله مره اخرى';
+            } 
+			callback(response);
+           } 
+          );
+     }
 
+    },
+	
+	
+	saveSchools: function(req,res,callback) {
+     var schoolsData = req.body.schoolsData;
+     var response = {};
+      con.query(" insert into SCH_School  (name, gender,educationLevel, address,totalClasses ,totalStudents ,totalStaff,rentedBuildings,governmentBuildings,foundationYear,logoFile) values(?)",
+          [schoolsData],function(err,result){
+            if(err)
+             throw err
+            if(result.affectedRows){
+             response.success = true;
+			 response.msg = 'تم الاضافه بنجاح'
+            }else{
+             response.success = false;
+			 response.msg = 'خطأ , الرجاء المحاوله مره اخرى';
+            } 
+			callback(response);
+           } 
+          );
     }
 };
 
