5c423d2e60bffc641bb6f328df74aa66564d532a
Salim Quta <salim@leannode.com>
2018-03-30 23:12:29 +0200

    reconnecting node.js

--- src/server/routes/dbConfig.js
+++ src/server/routes/dbConfig.js
@@ -1,14 +1,38 @@
 var mysql = require('mysql');
-var con = mysql.createConnection({
+let config = {
     host: "localhost",
     user: "root",
     password: "salimquta",
-    database:"schooltech"
-});
+    database: "schooltech"
+};
+let con;
 
-con.connect(function(err) {
+con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
 });
+
+function handleDisconnect() {
+    con = mysql.createConnection(config); // Recreate the connection, since
+    // the old one cannot be reused.
+
+    con.connect(function (err) {              // The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    con.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
 
 module.exports = con;
