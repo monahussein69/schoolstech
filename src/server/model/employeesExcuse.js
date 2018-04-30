3a76a0cc0126a1966f35a4220528b3f5cb7a6904
salim <salim.3bd@gmail.com>
2018-04-30 09:23:54 +0000

    changes

--- src/server/model/employeesExcuse.js
+++ src/server/model/employeesExcuse.js
@@ -16,18 +16,21 @@ var employeesExcuseMethods = {
                 ExcuseObj.Calender_id = calendarObj.Id;
                 ExcuseObj.Start_Date = moment(ExcuseObj.Start_Date).format('MM-DD-YYYY');
                 ExcuseObj.End_Date = moment(ExcuseObj.End_Date).format('MM-DD-YYYY');
-
+				var Event_Name = ExcuseObj.Event_Name;
+   
                 sequelizeConfig.employeeExcuseTable.find({where: {id: ExcuseObj.Calender_id}}).then(function (Excuse) {
                     if (Excuse) {
                         Excuse.updateAttributes(ExcuseObj).then(function () {
                             sequelizeConfig.employeeAttandaceTable.find({
                                 where: {
                                     employee_id: ExcuseObj.Emp_id,
-                                    Calender_id: ExcuseObj.Calender_id
+                                    Calender_id: ExcuseObj.Calender_id,
+									Event_Name:Event_Name
                                 }
                             })
                                 .then(function (employeeAttendence) {
                                     // Check if record exists in db
+									console.log(employeeAttendence);
                                     if (employeeAttendence) {
                                         employeeAttendence.updateAttributes({
                                             is_excused: 1
@@ -46,11 +49,13 @@ var employeesExcuseMethods = {
                             sequelizeConfig.employeeAttandaceTable.find({
                                 where: {
                                     employee_id: ExcuseObj.Emp_id,
-                                    Calender_id: ExcuseObj.Calender_id
+                                    Calender_id: ExcuseObj.Calender_id,
+									Event_Name:Event_Name
                                 }
                             })
                                 .then(function (employeeAttendence) {
                                     // Check if record exists in db
+									console.log(employeeAttendence);
                                     if (employeeAttendence) {
                                         employeeAttendence.updateAttributes({
                                             is_excused: 1
