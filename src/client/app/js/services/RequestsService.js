f92a343aa70871f36519e783c8d453a1f819e48e
Salim Quta <salim@leannode.com>
2018-05-03 14:26:02 +0300

    actions delay

--- src/client/app/js/services/RequestsService.js
+++ src/client/app/js/services/RequestsService.js
@@ -1,7 +1,7 @@
 angular.module('MetronicApp').factory('RequestsService', function ($http, Upload) {
     var service = {};
     service.saveRequestData = function (requestObj, callback) {
-        $http.post("http://localhost:3000/saveRequestData", {
+        $http.post("http://138.197.175.116:3000/saveRequestData", {
             'requestData': requestObj
         }).success(function (response) {
             callback(response);
@@ -9,27 +9,27 @@ angular.module('MetronicApp').factory('RequestsService', function ($http, Upload
     };
 
       service.changeStatusForRequests = function (status , id , callback) {
-          $http.post("http://localhost:3000/changeStatusForRequests", {id : id , status : status}).success(function (response) {
+          $http.post("http://138.197.175.116:3000/changeStatusForRequests", {id : id , status : status}).success(function (response) {
               callback(response);
           });
     };
 
     service.getEmployeeRequests = function (employee_id, callback) {
-        $http.get("http://localhost:3000/getEmployeeRequests/" + employee_id).success(function (response) {
+        $http.get("http://138.197.175.116:3000/getEmployeeRequests/" + employee_id).success(function (response) {
             callback(response);
         });
     };
 
     service.getSchoolRequests = function (school_id, callback) {
         console.log(school_id);
-        $http.get("http://localhost:3000/getSchoolRequests/" + school_id).success(function (response) {
+        $http.get("http://138.197.175.116:3000/getSchoolRequests/" + school_id).success(function (response) {
             callback(response);
         });
     };
 
     service.getRequestsTypes = function (callback) {
         console.log("get Requests ");
-        $http.get("http://localhost:3000/getRequestsTypes").success(function (response) {
+        $http.get("http://138.197.175.116:3000/getRequestsTypes").success(function (response) {
             console.log(response);
             callback(response);
         });
